d1bbb71950babd8e0b8a77d0116648cb4535dbf1
Guldana Baktybekova <guldanabaktybekova10@gmail.com>
2021-12-22 14:17:30 +0600

    added var to script.js

---
+++ script.js
@@ -0,0 +1,6 @@
+var example1 = 10 === 10;   //equals
+var exxample2 = 11 > 10; // more than
+var example3 = 10 < 11; //less than
+var example4 = 10 >= 10; //more than or equal
+var example5 = 10 <= 10; //less than or equal
+var example6 = 11 !== 10; //not equlas
