f9e34b68ca52a4de78a473a8584c4ecc4e2cfc3c
Guldana Baktybekova <guldanabaktybekova10@gmail.com>
2021-12-22 15:06:09 +0600

    Added promt ot script.js

--- script.js
+++ script.js
@@ -4,3 +4,78 @@ var example3 = 10 < 11; //less than
 var example4 = 10 >= 10; //more than or equal
 var example5 = 10 <= 10; //less than or equal
 var example6 = 11 !== 10; //not equlas
+
+var a = 5;
+var b = 10;
+var example7 = a < b;
+
+var c = 10;
+var d = 11;
+// if (c < d) { 
+//   alert("C is less than 0");
+//   console.log ('Hello world');
+// }
+
+var e = 3.14;
+// if (e !==3.14) {
+//   alert("Why don't you number PI");
+// }
+// else {
+//   alert("Good job!");
+// }
+
+// var age = 10;
+// if (age >= 18) {
+//   alert("You can't vote!"); 
+// }
+// else {
+//   alert("You are too young to vote!");
+// }
+
+
+// var lightSwitch = 1;
+// if (lightSwitch === 1) {
+//   alert("Light is on!");
+// }
+// else {
+//   alert("Light is off!")
+// }
+
+// var year = 2010;
+// var age = 2022 - year;
+// if (age >= 10) {
+//   alert("You can vote!");
+
+// }
+// else {
+//   alert("You are too young to vote!");
+// }
+
+
+// if (confirm("Do you have a car?")) {
+//   if (confirm("Is it red")) {
+//     alert("You have a red car.")
+//   }
+//   else {
+//     alert("You have a car that is not red.")
+//   }
+// }
+// else {
+//  if (confirm("Are you planning to have a car?")) {
+//    alert("You are planning to a car.")
+//  }
+//  else {
+//    alert("You don't like cars.")
+//  }
+// }
+
+
+// var username = prompt("What is your name?", "stranger");
+// if (username === " ") {
+//   username = "stranger";
+// }
+// alert("Welcome " + username);
+
+var a = prompt("What is the width of the rectangle?");
+var b = prompt("What is the height of the rectangle?");
+alert (a + b);
